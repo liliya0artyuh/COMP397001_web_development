674eb792260845bd183437aea65a31275f3c282e
liliya0artyuh <liliya0artyukh@gmail.com>
2015-10-04 20:38:47 -0400

    second commit

---
+++ Scripts/core/game.ts
@@ -0,0 +1,10 @@
+/// <reference path="../typings/createjs-lib.d.ts" />
+/// <reference path="../typings/easeljs.d.ts" />
+/// <reference path="../typings/preloadjs.d.ts" />
+/// <reference path="../typings/soundjs.d.ts" />
+/// <reference path="../typings/tweenjs.d.ts" />
+/// <reference path="../typings/waa.d.ts" />
+/// <reference path="../typings/mediastream.d.ts" />
+/// <reference path="../typings/rtcpeerconnection.d.ts" />
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
