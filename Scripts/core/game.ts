1d078be1784c45963e4a7574a10be7b5d7e39e21
liliya0artyuh <liliya0artyukh@gmail.com>
2015-10-04 21:51:25 -0400

    fourth commit

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -8,3 +8,38 @@
 /// <reference path="../typings/rtcpeerconnection.d.ts" />
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
+// global game framework variables
+var canvas: HTMLElement;
+var stage: createjs.Stage;
+
+// game variables
+var helloLabel: createjs.Text;
+
+
+function init() {
+	canvas = document.getElementById("canvas");// makes reference to canvas in html page
+	stage = new createjs.Stage(canvas); //associate the canvas element with the stage
+	createjs.Ticker.setFPS(60);// set the frame to 60 fps
+	createjs.Ticker.on("tick", gameLoop, this);
+	
+	main();
+}
+
+
+function gameLoop(event:createjs.Event):void {
+	stage.update();
+}
+
+function main():void {
+	console.log("we are in main method!");
+	helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#FFFF00");
+	// 0 point is top left corner
+	// draw to the right - positive x
+	// draw to the right - negative x
+	// draw up - negative y
+	// draw down - positive y
+	helloLabel.x = 320;
+	helloLabel.y = 240;
+	
+	stage.addChild(helloLabel);
+}
